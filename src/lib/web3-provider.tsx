89e01cecabd0ddd322c6bacf8348d5c5d7a0b9c2
kehinde Olusegun <olusegunkehindejones@gmail.com>
2025-11-27 14:36:51 +0000

    allow connection of wallet using reown appkit

--- src/lib/web3-provider.tsx
+++ src/lib/web3-provider.tsx
@@ -1,26 +1,37 @@
 'use client';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import { WagmiProvider, createConfig, http } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
-import { injected } from 'wagmi/connectors';
+import {WagmiProvider, createConfig, http} from 'wagmi';
+import {mainnet, sepolia} from 'wagmi/chains';
+import {AppKitProvider, getDefaultConfig} from '@reown/appkit';
 
-const config = createConfig({
-  chains: [mainnet, sepolia],
-  connectors: [injected()],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-});
+const config = createConfig(
+  getDefaultConfig({
+    appName: 'MicroPool dApp',
+    // Your project ID from WalletConnect Cloud
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    chains: [mainnet, sepolia],
+    transports: {
+      [mainnet.id]: http(),
+      [sepolia.id]: http(),
+    },
+  })
+);
 
 const queryClient = new QueryClient();
 
 export default function Web3Provider({children}: {children: React.ReactNode}) {
   return (
     <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppKitProvider
+          theme="dark"
+          options={{
+            overlayBlur: 'small',
+          }}
+        >
+          {children}
+        </AppKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
